a8675d515a2d36203713e3e3735ed87319523b08
MohHosameldin <mohamedhosameldin496@gmail.com>
2025-09-13 06:00:21 +0300

    Service add

--- src/main/java/com/example/studentsapi/Student/StudentRepo.java
+++ src/main/java/com/example/studentsapi/Student/StudentRepo.java
@@ -3,7 +3,8 @@ package com.example.studentsapi.Student;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
+@Repository
 public interface StudentRepo extends JpaRepository<Student, Long> {
 Optional<Student> findStudentByEmail(String email);
 Optional<Student> findStudentById(Long id);
---
+++ src/main/java/com/example/studentsapi/Student/StudentService.java
@@ -0,0 +1,5 @@
+package com.example.studentsapi.Student;
+
+public class StudentService {
+
+}
