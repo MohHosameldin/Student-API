81dc1dda887d31e0c5717267c126b19b604cd2dd
MohHosameldin <mohamedhosameldin496@gmail.com>
2025-09-13 20:49:16 +0300

    Service finished

--- src/main/java/com/example/studentsapi/Student/StudentService.java
+++ src/main/java/com/example/studentsapi/Student/StudentService.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import   org.springframework.transaction.annotation.Transactional;
+
 
 @Service
 public class StudentService {
@@ -19,7 +21,18 @@ public class StudentService {
     public Student findbyid(Long id) {
         return studentRepo.findStudentById(id).orElseThrow(()-> new IllegalStateException("Student with id "+id+" does not exist"));
     }
+public Student findbyemail(String email) {
+        return studentRepo.findStudentByEmail(email).orElseThrow(()-> new IllegalStateException("Student with email "+email+" does not exist"));
+    }
+    public Student findbynationalid(String NationalID) {
+        return studentRepo.findStudentByNationalID(NationalID).orElseThrow(()-> new IllegalStateException("Student with National ID "+NationalID+" does not exist"));
+    }
+
+
     public void addNewStudent(Student student) {
+         if (student == null) {
+        throw new IllegalArgumentException("Student cannot be null");
+    }
         if(studentRepo.findStudentByEmail(student.getEmail()).isPresent()){
             throw new IllegalStateException("Email already taken");
         }
@@ -31,6 +44,39 @@ public class StudentService {
         }
         studentRepo.save(student);
     }
-
+    public void deleteStudent(Long id) {
+        if(!studentRepo.existsById(id)){
+            throw new IllegalStateException("Student with id "+id+" does not exist");
+        }
+        studentRepo.deleteById(id);
+    }
+    public boolean isValidStringUpdate(String oldString , String newString) {
+        return newString != null && newString.length() > 0 && !newString.equals(oldString);
+    }
+    @Transactional
+    public void updateStudent(Long id, String FirstName, String LastName, String email, String Address, String PhoneNumber) {
+        Student student = studentRepo.findById(id).orElseThrow(()-> new IllegalStateException("Student with id "+id+" does not exist"));
+        if(FirstName != null && FirstName.length()>0 && !FirstName.equals(student.getFirstName())){
+            student.setFirstName(FirstName);
+        }
+        if(LastName != null && LastName.length()>0 && !LastName.equals(student.getLastName())){
+            student.setLastName(LastName);
+        }
+        if(email != null && email.length()>0 && !email.equals(student.getEmail())){
+            if(studentRepo.findStudentByEmail(email).isPresent()){
+                throw new IllegalStateException("Email already taken");
+            }
+            student.setEmail(email);
+        }
+        if(Address != null && Address.length()>0 && !Address.equals(student.getAddress())){
+            student.setAddress(Address);
+        }
+        if(PhoneNumber != null && PhoneNumber.length()>0 && !PhoneNumber.equals(student.getPhoneNumber())){
+            if(studentRepo.findStudentByPhoneNumber(PhoneNumber).isPresent()){
+                throw new IllegalStateException("Phone number already taken");
+            }
+            student.setPhoneNumber(PhoneNumber);
+        }
+    }
 
 }
